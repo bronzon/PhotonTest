d9d6aa26320cb7c7549b51ae48bf72c642873914
bronzon <olle.back@gmail.com>
2017-05-14 20:46:36 +0200

    fix stupid merge

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager  : Photon.PunBehaviour {
+	public static GameManager INSTANCE;
+	public GameObject playerPrefab;
+
+	void Start()  {
+		INSTANCE = this;
+		PhotonNetwork.Instantiate (playerPrefab.name, new Vector3 (0f, 0.5f, 0f), Quaternion.identity, 0);
+	}
+
 	public override void OnLeftRoom () {
 		SceneManager.LoadScene ("Launcher");
 	}
@@ -13,12 +21,25 @@ public class GameManager  : Photon.PunBehaviour {
 	}
 
 	public void SwitchLevel() {
-		int index = SceneManager.GetActiveScene ().buildIndex;
-
-		if (index == 1) {
-			SceneManager.LoadScene (2);
-		} else {
-			SceneManager.LoadScene (1);
+		if (PhotonNetwork.isMasterClient) {
+			int index = SceneManager.GetActiveScene ().buildIndex;
+			if (index == 1) {
+				SceneManager.LoadScene (2);
+			} else {
+				SceneManager.LoadScene (1);
+			}
 		}
+	}
+
+	public override void OnPhotonPlayerConnected (PhotonPlayer newPlayer) {
+		Debug.Log( "OnPhotonPlayerConnected() " + newPlayer.NickName );
+		if (PhotonNetwork.isMasterClient) {
+			//blabla
+		}
+	}
+
+	public override void OnPhotonPlayerDisconnected (PhotonPlayer otherPlayer) {
+		Debug.Log( "OnPhotonPlayerConnected() " + otherPlayer.NickName );
+		//blabla
 	}
 }
