5aafe0f19c520fd08c8b9c4a5bf9fcc0331c9132
bronzon <olle.back@gmail.com>
2017-05-14 20:32:59 +0200

    synchronizing transforms, health and firing

---
+++ Assets/AnimationController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : Photon.MonoBehaviour {
+	Animator animator;
+	public float directionDampening = 0.25f;
+	// Use this for initialization
+	void Start () {
+		this.animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!photonView.isMine && PhotonNetwork.connected) {
+			return;
+		}
+
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+
+		if (v < 0) {
+			v = 0;
+		}
+		print(h*h + v*v);
+		animator.SetFloat ("Speed", h * h + v * v);
+		animator.SetFloat ("Direction", h, directionDampening, Time.deltaTime);
+
+		var animatorStateInfo = animator.GetCurrentAnimatorStateInfo (0);
+		if (animatorStateInfo.IsTag ("run") && Input.GetButtonDown("Fire2")) {
+			animator.SetTrigger ("Jump");
+		}
+
+	}
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ExitGames.Demos.DemoAnimator;
+
+public class PlayerController : Photon.PunBehaviour, IPunObservable {
+
+	public GameObject beams;
+	public float health = 1.0f;
+
+	bool isFiring;
+
+	void Awake() {		
+		beams.SetActive(false);
+	}
+
+	void Start() {
+		CameraWork cameraWork = GetComponent<CameraWork> ();
+		if (photonView.isMine || !PhotonNetwork.connected) {
+			cameraWork.OnStartFollowing ();
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (!photonView.isMine) {
+			return;
+		}
+
+		if (other.tag == "beam") {
+			health -= 0.1f;
+		}
+	}
+
+	void OnTriggerStay(Collider other) {
+		if (!photonView.isMine) {
+			return;
+		}
+
+		if (other.tag == "beam") {
+			health -= 0.1f * Time.deltaTime;
+		}
+	}
+
+
+	void Update() {
+		if (photonView.isMine) {
+			ProcessInputs ();
+		}
+
+
+		if (isFiring != beams.GetActive ()) {
+			beams.SetActive(isFiring);
+		}
+
+		if (health <= 0) {
+			GameManager.INSTANCE.QuitGame ();
+		}
+	}
+
+	public void OnPhotonSerializeView (PhotonStream stream, PhotonMessageInfo info) {
+		if (stream.isWriting) {
+			stream.SendNext (isFiring);
+			stream.SendNext (health);
+		} else {
+			isFiring = (bool)stream.ReceiveNext ();
+			health = (float)stream.ReceiveNext ();
+		}
+	}
+
+	void ProcessInputs() {
+
+		if (Input.GetButtonDown ("Fire1") ) {
+			if (!isFiring) {
+				isFiring = true;
+			}
+		}
+
+		if (Input.GetButtonUp ("Fire1") ) {
+			if (isFiring) {
+				isFiring = false;
+			}
+		}
+	}
+
+}
